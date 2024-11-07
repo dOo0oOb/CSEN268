0cff3e5e403daf6023f35e51ed8f133448aad675
dOo0oOb <kikicoco777us@gmail.com>
2024-11-07 11:16:28 -0800

    added tabs for homepage, added hero widgets, changed structure for all widgets

--- Implicit_Animations/lib/pages/my_home_page.dart
+++ Implicit_Animations/lib/pages/my_home_page.dart
@@ -1,64 +1,112 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../widgets/AnimatedAlignExample.dart';
+import '../widgets/AnimatedContainerExample.dart';
+import '../widgets/AnimatedCrossFadeExample.dart';
+import '../widgets/AnimatedOpacityExample.dart';
+import '../widgets/AnimatedRotationExample.dart';
 import '../widgets/AnimatedSwitcherExample.dart';
-import '../widgets/AnimatedAlign.dart';
+import '../widgets/HeroExample.dart';
 
-import '../widgets/padded_text.dart';
-import '../widgets/AnimatedRotation.dart';
-import '../widgets/AnimatedContainer.dart';
-import '../widgets/AnimatedOpacity.dart';
-import '../widgets/AnimatedAlign.dart';
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key, required this.title});
+//   final String title;
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: const Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             SizedBox(
+//               height: 200, // Give a fixed height or wrap with Flexible
+//               child: AnimatedSwitcherExample(),
+//             ),
+//             SizedBox(
+//               height: 200,
+//               child: Rotation(),
+//             ),
+//             SizedBox(
+//               height: 200,
+//               child: AnimatedContainerExampleApp(),
+//             ),
+//             SizedBox(
+//               height: 200,
+//               child: CombinedOpacityExampleApp(),
+//             ),
+//             SizedBox(
+//               height: 200,
+//               child: AnimatedAlignExampleApp(),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key, required this.title,}) : super(key: key);
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<Widget> animationExamples = [
+    const AnimatedSwitcherExample(),
+    const HeroExample(),
+    const AnimatedRotationExample(),
+    const AnimatedContainerExample(),
+    const CombinedOpacityExample(),
+    const AnimatedAlignExample(),
+    const CrossFadeExample(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Implicit Animation Examples'),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 200, // Give a fixed height or wrap with Flexible
-              child: AnimatedSwitcherExample(),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8.0), // Add padding around the ListView
+        itemCount: animationExamples.length,
+        itemBuilder: (context, index) {
+          final widgetName = animationExamples[index].runtimeType.toString();
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0), // Add padding between each card
+            child: Card(
+              color: const Color.fromARGB(255, 179, 206, 234),
+              child: ListTile(
+                title: Text(widgetName),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => animationExamples[index]),
+                  );
+                },
+              ),
             ),
-            SizedBox(
-              height: 200,
-              child: Rotation(),
-            ),
-            SizedBox(
-              height: 200,
-              child: AnimatedContainerExampleApp(),
-            ),
-            SizedBox(
-              height: 200,
-              child: CombinedOpacityExampleApp(),
-            ),
-            SizedBox(
-              height: 200,
-              child: AnimatedAlignExampleApp(),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
--- Implicit_Animations/lib/widgets/AnimatedSwitcherExample.dart
+++ Implicit_Animations/lib/widgets/AnimatedSwitcherExample.dart
@@ -1,59 +1,117 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class AnimatedSwitcherExample extends StatefulWidget {
   const AnimatedSwitcherExample({super.key});
 
   @override
-  State<AnimatedSwitcherExample> createState() =>
-      _AnimatedSwitcherExampleState();
+  State<AnimatedSwitcherExample> createState() => _AnimatedSwitcherExampleState();
 }
 
 class _AnimatedSwitcherExampleState extends State<AnimatedSwitcherExample> {
-  int _count = 100;
+  int _count = 10;
   late Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    // Start a timer that decreases the counter every second
+    _startTimer();
+  }
+
+  void _startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         if (_count > 0) {
           _count--;
         } else {
-          _timer.cancel(); // Stop the timer when _count reaches 0
+          _timer.cancel();
         }
       });
     });
   }
 
+  void _resetTimer() {
+    _timer.cancel();  // Stop the current timer
+    setState(() {
+      _count = 10;  // Reset count to initial value
+    });
+    _startTimer();  // Start the timer again
+  }
+
   @override
   void dispose() {
-    _timer.cancel(); // Cancel the timer when the widget is disposed
+    _timer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Animated Switcher Example')),
+      appBar: AppBar(
+        title: const Text('AnimatedSwitcher'),
+        centerTitle: true,
+      ),
       body: ColoredBox(
         color: Colors.white,
         child: Center(
-          child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 500),
-            transitionBuilder: (Widget child, Animation<double> animation) {
-              return ScaleTransition(scale: animation, child: child);
-            },
-            child: Text(
-              '$_count',
-              key: ValueKey<int>(_count),
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'duration',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 10000),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+                child: Text(
+                  '$_count',
+                  key: ValueKey<int>(_count),
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'non-unique key',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return ScaleTransition(scale: animation, child: child);
+                },
+                child: Text(
+                  '$_count',
+                  key: ValueKey<int>(0),
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'unique key',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return ScaleTransition(scale: animation, child: child);
+                },
+                child: Text(
+                  '$_count',
+                  key: ValueKey<int>(_count),
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ),
+            ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _resetTimer,
+        tooltip: 'Reset Timer',
+        child: const Icon(Icons.refresh),
       ),
     );
   }
