faf169ba1a36d35827ae386bc3f490fdf09d8530
dOo0oOb <kikicoco777us@gmail.com>
2024-11-11 01:09:32 -0800

    roll back

---
+++ Implicit_Animations/lib/widgets/AnimatedAlignExample.dart
@@ -0,0 +1,100 @@
+// import 'package:flutter/material.dart';
+
+// class AnimatedAlignExampleApp extends StatelessWidget {
+//   const AnimatedAlignExampleApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Combined AnimatedAlign Example')),
+//         body: const AnimatedAlignExample(),
+//       ),
+//     );
+//   }
+// }
+
+// class AnimatedAlignExample extends StatefulWidget {
+//   const AnimatedAlignExample({super.key});
+
+//   @override
+//   State<AnimatedAlignExample> createState() => _AnimatedAlignExampleState();
+// }
+
+// class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
+//   bool isTopLeft = false;
+
+//   void _toggleAlignment() {
+//     setState(() {
+//       isTopLeft = !isTopLeft;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: _toggleAlignment,
+//       child: Center(
+//         child: Container(
+//           width: 250.0,
+//           height: 250.0,
+//           color: Colors.red,
+//           child: AnimatedAlign(
+//             alignment: isTopLeft ? Alignment.topLeft : Alignment.bottomRight,
+//             duration: const Duration(seconds: 1),
+//             curve: Curves.fastOutSlowIn,
+//             child: const FlutterLogo(size: 50.0),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+import 'package:flutter/material.dart';
+
+class AnimatedAlignExample extends StatefulWidget {
+  const AnimatedAlignExample({super.key});
+
+  @override
+  State<AnimatedAlignExample> createState() => _AnimatedAlignExampleState();
+}
+
+class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
+  bool isTopLeft = false;
+
+  void _toggleAlignment() {
+    setState(() {
+      isTopLeft = !isTopLeft;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AnimatedAlign'),
+      centerTitle: true,
+      ),
+      body: GestureDetector(
+        onTap: _toggleAlignment,
+        child: Center(
+          child: Container(
+            width: 250.0,
+            height: 250.0,
+            color: Colors.red,
+            child: AnimatedAlign(
+              alignment: isTopLeft ? Alignment.topLeft : Alignment.bottomRight,
+              duration: const Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn,
+              child: const FlutterLogo(size: 50.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
